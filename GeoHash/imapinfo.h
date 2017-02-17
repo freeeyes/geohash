145be237a5e35ab8a9c7fd51f3acdf01833c6f4c
freeeyes <freeeyes@163.com>
2017-02-17 09:14:46 +0800

    上传指定的文件

---
+++ GeoHash/imapinfo.h
@@ -0,0 +1,22 @@
+#ifndef _IMAP_INFO
+#define _IMAP_INFO
+
+#include "posinfopool.h"
+#include <vector>
+#include <string>
+using namespace std;
+
+//add by freeeyes
+//用于提供模块的虚接口
+
+class IMapInfo
+{
+public:
+	IMapInfo() {};
+	virtual ~IMapInfo() {};
+	
+	virtual bool AddPos(const char* pMsisdn, double dPosLatitude, double dPosLongitude, time_t ttPos) = 0;
+	virtual bool FindPos(double dPosLatitude, double dPosLongitude, double dDistance, vector<_Pos_Info*>& vecPosList) = 0;
+};
+
+#endif
